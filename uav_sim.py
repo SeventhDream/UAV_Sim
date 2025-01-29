7074801c3088f0d8da2a8dc4f5fc2367fe7e4b8c
Reuel Terezakis <68449189+SeventhDream@users.noreply.github.com>
2025-01-29 15:24:09 +0400

    FEATURE: Add thrust control using arrow keys.
    Known Issues:
    - Drone rotates uncontrolleable when thrust is applied.

--- uav_sim.py
+++ uav_sim.py
@@ -12,16 +12,32 @@ p.setGravity(0, 0, -9.81) # Earth's gravity.
 planeId = p.loadURDF("plane.urdf")
 
 # Initialise UAV.
-uav_path = os.path.abspath("cf2x.urdf")
-start_pos = [0, 0, 2] # UAV starts at (0,0) at 2m height.
-start_orientation = p.getQuaternionFromEuler([0, 0, 0]) # No rotation.
+uavPath = os.path.abspath("cf2x.urdf")
+startPos = [0, 0, 2] # UAV starts at (0,0) at 2m height.
+startOrientation = p.getQuaternionFromEuler([0, 0, 0]) # No rotation.
 
 # Load UAV model.
-uavId = p.loadURDF(uav_path, start_pos, start_orientation) # Temporary UAV model.
+uavId = p.loadURDF(uavPath, startPos, startOrientation) # Temporary UAV model.
+
+# Keyboard Input.
+def get_keyboard_control():
+    keys = p.getKeyboardEvents()
+    thrust = 0
+
+    # Control thrusts with up and down arrow keys.
+    if p.B3G_UP_ARROW in keys and keys[p.B3G_UP_ARROW]:
+        thrust = 5
+    elif p.B3G_DOWN_ARROW in keys and keys[p.B3G_DOWN_ARROW]:
+        thrust = -5
+
+    return thrust
 
 # Simulation loop.
-for _ in range(1000):
-    p.stepSimulation() # Run physics calculations.
-    time.sleep(1 / 240.0) # Simulate at 240 Hz
+while True:
+    thrust = get_keyboard_control()
 
-p.disconnect()
+    # Apply force (simulating rotor thrust).
+    p.applyExternalForce(uavId, -1, [0, 0, thrust], [0, 0, 0], p.WORLD_FRAME)
+
+    p.stepSimulation()
+    time.sleep(1/100.0) # Run at 100 Hz.
