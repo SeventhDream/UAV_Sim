d4acc89c679477d07db6ab81b1dc988e54bd07f4
Reuel Terezakis <68449189+SeventhDream@users.noreply.github.com>
2025-01-29 15:42:25 +0400

    FEATURE: Replace keyboard input with PID altitude control.
    - Implement PID controller class and use it to control thurst to
      maintain desired altitude in metres.

--- uav_sim.py
+++ uav_sim.py
@@ -3,6 +3,23 @@ import pybullet_data
 import time
 import os
 
+class PIDController:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+        self.prev_error = 0
+        self.integral = 0
+
+    def compute(self, setPoint, measuredValue, dt):
+        error = setPoint - measuredValue
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt
+        self.prev_error = error
+
+        return (self.kp * error) + (self.ki *self.integral) + (self.kd * derivative)
+
 # Initialise physics simulation.
 physicsClient = p.connect(p.GUI) # GUI mode for visualisation.
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) # Load built-in assets.
@@ -20,21 +37,26 @@ startOrientation = p.getQuaternionFromEuler([0, 0, 0]) # No rotation.
 uavId = p.loadURDF(uavPath, startPos, startOrientation) # Temporary UAV model.
 
 # Keyboard Input.
-def get_keyboard_control():
-    keys = p.getKeyboardEvents()
-    thrust = 0
+# def get_keyboard_control():
+#     keys = p.getKeyboardEvents()
+#     thrust = 0
 
-    # Control thrusts with up and down arrow keys.
-    if p.B3G_UP_ARROW in keys and keys[p.B3G_UP_ARROW]:
-        thrust = 5
-    elif p.B3G_DOWN_ARROW in keys and keys[p.B3G_DOWN_ARROW]:
-        thrust = -5
+#     # Control thrusts with up and down arrow keys.
+#     if p.B3G_UP_ARROW in keys and keys[p.B3G_UP_ARROW]:
+#         thrust = 5
+#     elif p.B3G_DOWN_ARROW in keys and keys[p.B3G_DOWN_ARROW]:
+#         thrust = -5
 
-    return thrust
+#     return thrust
 
+pidAltitude = PIDController(1.5, 0.02, 0.1)
+targetAltitude = 2.0 # Desired Altitude.
 # Simulation loop.
 while True:
-    thrust = get_keyboard_control()
+    pos, _ = p.getBasePositionAndOrientation(uavId)
+    altitude = pos[2]
+
+    thrust = pidAltitude.compute(targetAltitude, altitude, 1/100.0)
 
     # Apply force (simulating rotor thrust).
     p.applyExternalForce(uavId, -1, [0, 0, thrust], [0, 0, 0], p.WORLD_FRAME)
